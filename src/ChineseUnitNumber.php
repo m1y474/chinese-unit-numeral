46679ed7414b602e65f06ff1652213df5178a93c
miyata <rm.eilsystem@gmail.com>
2021-11-07 18:28:22 +0900

    big bang

---
+++ src/ChineseUnitNumber.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace ChineseUnitNumber;
+
+/**
+ * 漢数字の単位をつける
+ */
+class ChineseUnitNumber
+{
+    /**
+     * 漢数字の単位
+     *
+     * @var array
+     */
+    const UNITS = ['京', '兆', '億', '万', ''];
+
+    /**
+     * 数値を漢数字に変換する
+     *
+     * @param integer $value
+     * @param boolean $isFormat
+     *
+     * @return string
+     *
+     * @SuppressWarnings(PHPMD.BooleanArgumentFlag)
+     */
+    public function convert(int $value, $isFormat = true)
+    {
+        // 1000以下の場合は単位を付けずに返却
+        if ((int) $value < 1000) {
+            return (string) $value;
+        }
+
+        // 桁数によって使用する単位の調整
+        $units = self::UNITS;
+        switch ($value) {
+        case $value < 100000000:
+            $units = array_splice($units, 3);
+            break;
+        case $value < 1000000000000:
+            $units = array_splice($units, 2);
+            break;
+        case $value < 10000000000000000:
+            $units = array_splice($units, 1);
+            break;
+        }
+
+        // 右から桁数を分割していく
+        $splits = str_split(strrev($value), 4);
+        $convert = '';
+        // 反転した数値を元に戻して単位をつける
+        foreach (array_reverse($splits) as $index => $split) {
+            // フォーマットしない
+            if (!$isFormat) {
+                $convert .= strrev($split) . $units[$index];
+                continue;
+            }
+            // 0000の場合、表記を省略する
+            $convert .= (int) $split === 0 ? '' : number_format(strrev($split)) . $units[$index];
+        }
+
+        return $convert;
+    }
+}
---
+++ tests/ChineseUnitNumberTest.php
@@ -0,0 +1,32 @@
+<?php
+
+use ChineseUnitNumber\ChineseUnitNumber;
+use PHPUnit\Framework\TestCase;
+
+class ChineseUnitNumberTest extends TestCase
+{
+    /**
+     * @dataProvider values
+     */
+    public function test漢数字に変換される($excepted, $value, $format=true)
+    {
+        $this->assertSame($excepted, (new ChineseUnitNumber())->convert($value, $format));
+    }
+
+    public function values()
+    {
+        return [
+            ['1億', '100000000'],
+            ['10兆', '10000000000000'],
+            ['100京', '1000000000000000000'],
+            ['1,001万100', '10010100'],
+            ['0', '0'],
+            ['200', '200'],
+            ['1億3万4,000', '100034000'],
+            ['1億0003万4000', '100034000', false],
+            ['1京1', '10000000000000001'],
+            ['1京0000兆0000億0000万0001', '10000000000000001', false],
+            ['1,123億400万', '112304000000'],
+        ];
+    }
+}
